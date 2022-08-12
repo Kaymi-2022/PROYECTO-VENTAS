dfda2b5c0e016c096c8b4a26abef809c356a59e5
KYM <kaymilyg2022@gmail.com>
2022-08-12 09:54:35 -0500

    TERMINDADO MAQUETACION

--- VISTA/JAVASCRIPT/modal.js
+++ VISTA/JAVASCRIPT/modal.js
@@ -5,8 +5,10 @@ function changeStyle() {
 }
 
 function cerrarVentana() {
-    document.querySelector(".contenedor-modal").style.visibility = "hidden";
-    document.querySelector(".contenedor-modal").style.opacity = "0";
     document.querySelector(".modal-navegacontenedor").style.transform="translateY(-200%)";
+
+    document.querySelector(".contenedor-modal").style.opacity = "0";
+    document.querySelector(".contenedor-modal").style.transition = "4s";
+    document.querySelector(".contenedor-modal").style.visibility = "hidden";
 }
 
---
+++ VISTA/JAVASCRIPT/modal2.js
@@ -0,0 +1,4 @@
+function cerrarLogin() {
+    document.querySelector(".contenedor-login").style.transition = "transform 4s";
+    document.querySelector(".contenedor-login").style.transform = "translateY(-200px)";
+}
